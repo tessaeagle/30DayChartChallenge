b5a7672e3478b2859d9de620108b0cbb2374c657
tessaeagle <eagle.tessa@gmail.com>
2021-04-13 13:10:01 -0700

    Add files via upload

--- Code/Day_12.R
+++ Code/Day_12.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
 df <- tibble(
-  date = seq(1:12),
-  done = c("yes","yes","yes","yes","yes","yes","yes","no","no","no","no","no"),
-  y = rep(1,12)
+  date = seq(1:13),
+  done = c("yes","yes","yes","yes","yes","yes","yes","no","no","no","no","yes","no"),
+  y = rep(1,13)
 )
 
 
@@ -11,10 +11,10 @@ p<-ggplot(df, aes(x = date, y = y, fill = done))+
   geom_tile(aes(width=0.5), color = "black")+
   scale_fill_manual(values = c("#f0b99e", "#95f2f5"))+
   annotate("text", x = 1.9, y = 1.55, label = "COMPLETED", color = "#95f2f5", size = 5)+
-  annotate("text", x = 10.6, y = 1.55, label = " NOT COMPLETED", color = "#f0b99e", size = 5)+
+  annotate("text", x = 9.4, y = 1.55, label = " NOT COMPLETED", color = "#f0b99e", size = 5)+
   annotate("text", x = 5, y = 1.62, label = "Adopted a dog", hjust = 0, size = 4, color = "white") +
   geom_curve(aes(x = 7, xend = 7.7, y = 1.6, yend = 1.5), curvature = -.2, size = .5, color = "white", arrow = arrow(length = unit(0.01, "npc"))) +
-  scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10,11,12))+
+  scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10,11,12,13))+
   theme(
     panel.grid = element_blank(),
     panel.background = element_rect(fill = "#101a35"),
